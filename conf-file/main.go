e3c471be16be0ac8403e3be8e9b6fd189487be4b
kmtc44 <kairmor@yahoo.com>
2020-09-21 12:09:35 +0000

    toml config file

--- conf-file/main.go
+++ conf-file/main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 
+	"github.com/BurntSushi/toml"
 	"github.com/go-yaml/yaml"
 )
 
@@ -16,16 +17,18 @@ func main() {
 			Port string `json:"port" yaml:"port"`
 		} `json:"database" yaml:"database"`
 		User struct {
-			Username string `json:"login" yaml:"login"`
+			Username string `json:"login" yaml:"login" toml:"login"`
 			Password string `json:"password" yaml:"password"`
 		} `json:"user" yaml:"user"`
 	}
 
 	var configjson Config
 	var configyaml Config
+	var configtoml Config
 
 	filejson, err := os.Open("config.json")
 	fileYaml, err := os.Open("config.yaml")
+	fileToml, err := os.Open("config.toml")
 
 	if err != nil {
 		fmt.Println(err)
@@ -33,18 +36,22 @@ func main() {
 
 	defer filejson.Close()
 	defer fileYaml.Close()
+	defer fileToml.Close()
 
 	content, err := ioutil.ReadAll(fileYaml)
+	content2, err := ioutil.ReadAll(fileToml)
 
 	if err != nil {
 		fmt.Println(err)
 	}
 	err = yaml.Unmarshal(content, &configyaml)
+	err = toml.Unmarshal(content2, &configtoml)
 
 	jsonDecoder := json.NewDecoder(filejson)
 	jsonDecoder.Decode(&configjson)
 
 	fmt.Println(configjson)
 	fmt.Println(configyaml)
+	fmt.Println(configtoml)
 
 }
